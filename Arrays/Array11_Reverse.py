c74a32a6693d59b7bd359b76bb53facab36807d3
vineeta786 <36797908+vineeta786@users.noreply.github.com>
2020-10-01 16:07:29 +0530

    Create Array11_Reverse.py

---
+++ Arrays/Array11_Reverse.py
@@ -0,0 +1,9 @@
+try:
+    arr = [int(x) for x in input().split()]
+    print("Original array: ",end=" ")
+    print(arr)
+    arr = arr[::-1]
+    print("Reversed array is: ",end=" ")
+    print(arr)
+except:
+    passa
